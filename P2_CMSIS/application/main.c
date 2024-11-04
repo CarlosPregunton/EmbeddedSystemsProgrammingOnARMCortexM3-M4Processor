d501470b9e1cdc8cab78d63de5aac29e086cc4cc
Carlos <jcsanchezg1215@gmail.com>
2024-11-03 18:06:21 -0600

    adding blinking led using cmsis (completed) and hal driver (in progress).

---
+++ P2_CMSIS/application/main.c
@@ -0,0 +1,22 @@
+#include <stdint.h>
+#include "stm32f3xx.h"
+
+#define LED_PIN 5
+
+void main(void)
+{
+  RCC->AHBENR |= (1 << RCC_AHBENR_GPIOAEN_Pos);
+  
+  // do two dummy reads after enabling the peripheral clock, as per the errata
+  volatile uint32_t dummy;
+  dummy = RCC->AHBENR;
+  dummy = RCC->AHBENR;
+
+  GPIOA->MODER |= (1 << GPIO_MODER_MODER5_Pos);
+  
+  while(1)
+  {
+    GPIOA->ODR ^= (1 << LED_PIN);
+    for (uint32_t i = 0; i < 1000000; i++);
+  }
+}
---
+++ P3_HAL/Resourses.py
@@ -0,0 +1,59 @@
+"""
+STMicroelectronics mantiene varios repositorios públicos en GitHub 
+que contienen los SDK y bibliotecas para sus microcontroladores. 
+Los más relevantes son:
+
+    STM32Cube: Estos repositorios incluyen el firmware, 
+    las bibliotecas de periféricos, ejemplos de código y drivers 
+    para cada serie de microcontroladores STM32. Cada serie tiene 
+    su propio repositorio:
+
+        STM32CubeF4 para la serie STM32F4.
+        STM32CubeF3 para la serie STM32F3.
+        STM32CubeF1 para la serie STM32F1.
+
+        Y así sucesivamente para otras series como F0, F2, F7, G0, 
+        L0, etc.
+
+        Core CMSIS: https://github.com/ARM-software/CMSIS_5/blob/develop/CMSIS/Core/Include/cmsis_gcc.h 
+        specific MCU header: https://github.com/STMicroelectronics/cmsis-device-f3/blob/master/Include/stm32f303xe.h
+                            https://github.com/STMicroelectronics/cmsis-device-f3/blob/master/Include/stm32f3xx.h
+        startup file: https://github.com/STMicroelectronics/cmsis-device-f3/blob/master/Source/Templates/gcc/startup_stm32f303xe.s
+        system file: https://github.com/STMicroelectronics/cmsis-device-f3/blob/master/Source/Templates/system_stm32f3xx.c
+
+Para completar un proyecto básico usando las librerías HAL de ST 
+para el STM32F303RE, además de los archivos que ya identificaste, 
+necesitarás algunos adicionales para la correcta inicialización del 
+microcontrolador, el manejo de periféricos y las configuraciones 
+de sistema. Aquí te dejo una lista de archivos recomendados:
+
+1. Archivos de la HAL (Hardware Abstraction Layer)
+
+    stm32f3xx_hal.h: Archivo de encabezado principal de la HAL para la serie F3.
+    stm32f3xx_hal_conf.h: Archivo de configuración que define los módulos y funciones de la HAL que deseas habilitar.
+    stm32f3xx_hal_rcc.h y stm32f3xx_hal_rcc.c: Para la configuración del reloj (RCC).
+    
+    Otros archivos HAL específicos de los periféricos que uses, como:
+        stm32f3xx_hal_gpio.h y stm32f3xx_hal_gpio.c: Para el manejo de GPIO.
+        stm32f3xx_hal_uart.h y stm32f3xx_hal_uart.c: Para UART, si necesitas comunicación serie.
+        stm32f3xx_hal_tim.h y stm32f3xx_hal_tim.c: Para temporizadores.
+        Agrega archivos similares según los periféricos que uses.
+
+2. Archivos de CMSIS
+
+    core_cm4.h: Proporciona las definiciones específicas del núcleo Cortex-M4.
+    cmsis_version.h: Para controlar la versión de CMSIS utilizada.
+    Otros archivos CMSIS según el compilador y núcleo utilizados (ya has incluido cmsis_gcc.h).
+
+3. Archivos de Sistema y Startup
+
+    system_stm32f3xx.h: Encabezado para la configuración del sistema.
+    system_stm32f3xx.c: Archivo que configura el sistema y el reloj.
+    startup_stm32f303xe.s: Archivo de inicio que incluye el vector de 
+    interrupciones y el código de arranque. Ya lo tienes, pero confirma 
+    que esté configurado para el núcleo M4.
+
+4. Makefile
+
+    Crea un Makefile que establezca correctamente las rutas de los archivos CMSIS, HAL, e incluya los flags necesarios para el compilador, las opciones de optimización y los archivos .c o .s relevantes. Recuerda incluir las rutas de los encabezados de CMSIS y HAL.
+"""
