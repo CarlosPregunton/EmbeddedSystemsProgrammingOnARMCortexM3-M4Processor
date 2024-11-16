26cb7bf0c81decd008336f8b984af362c412cf5e
Carlos <jcsanchezg1215@gmail.com>
2024-11-16 17:42:53 -0600

    refactoring code.

--- P3_HAL/Drivers/Src/stm32f3xx_hal_msp.c
+++ P3_HAL/Drivers/Src/stm32f3xx_hal_msp.c
@@ -1,101 +1,99 @@
-/**
-  ******************************************************************************
-  * @file    stm32f3xx_hal_msp_template.c
-  * @author  MCD Application Team
-  * @brief   HAL MSP module.
-  *          This file template is located in the HAL folder and should be copied 
-  *          to the user folder.
-  *
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2016 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  @verbatim
-  ===============================================================================
-                     ##### How to use this driver #####
-  ===============================================================================
-    [..]
-
-  @endverbatim
-  ******************************************************************************
-  */ 
-
-/* Includes ------------------------------------------------------------------*/
 #include "stm32f3xx_hal.h"
-
-/** @addtogroup STM32F3xx_HAL_Driver
-  * @{
-  */
-
-/** @defgroup HAL_MSP HAL MSP module
-  * @brief HAL MSP module.
-  * @{
-  */
-
-/* Private typedef -----------------------------------------------------------*/
-/* Private define ------------------------------------------------------------*/
-/* Private macro -------------------------------------------------------------*/
-/* Private variables ---------------------------------------------------------*/
-/* Private function prototypes -----------------------------------------------*/
-/* Exported functions ---------------------------------------------------------*/
-
-/** @defgroup HAL_MSP_Exported_Functions HAL MSP Exported Functions
-  * @{
-  */
-
+#include "stm32f3xx_hal_msp.h"
 /**
-  * @brief  Initializes the Global MSP.
+  * @brief  This function is executed in case of error occurrence.
+  * @param  None
   * @retval None
   */
-void HAL_MspInit(void)
+static void Error_Handler(void)
 {
-   
+  /* User may add here some code to deal with this error */
+  while(1)
+  {
+  }
 }
 
 /**
-  * @brief  DeInitializes the Global MSP.
+  * @brief  System Clock Configuration
+  *         The system Clock is configured as follow : 
+  *            System Clock source            = PLL (HSI)
+  *            SYSCLK(Hz)                     = 64000000
+  *            HCLK(Hz)                       = 64000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 2
+  *            APB2 Prescaler                 = 1
+  *            HSI Frequency(Hz)              = 8000000
+  *            PREDIV                         = RCC_PREDIV_DIV2 (2)
+  *            PLLMUL                         = RCC_PLL_MUL16 (16)
+  *            Flash Latency(WS)              = 2
+  * @param  None
   * @retval None
   */
+void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  
+  /* HSI Oscillator already ON after system reset, activate PLL with HSI as source */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_NONE;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV2;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL16;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct)!= HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 
+     clocks dividers */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;  
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2)!= HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+// Inicializa el pin A5 como salida para el LED
+void LED_Init(void) {
+    __HAL_RCC_GPIOA_CLK_ENABLE();  // Habilita el reloj para el puerto A
+    
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    GPIO_InitStruct.Pin = GPIO_PIN_5;           // Configura el pin A5
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP; // Modo salida push-pull
+    GPIO_InitStruct.Pull = GPIO_NOPULL;         // Sin resistencias pull-up/down
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW; // Baja velocidad (suficiente para LEDs)
+    
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);     // Inicializa el pin con las configuraciones
+}
+
+void HAL_MspInit(void){
+    
+    /* Configure the system clock to 64 MHz */
+    SystemClock_Config();
+    
+    HAL_InitTick(TICK_INT_PRIORITY);
+    
+    /*Setup led pin*/
+    LED_Init(); 
+}
+
 void HAL_MspDeInit(void)
 {
   
 }
 
-/**
-  * @brief  Initializes the PPP MSP.
-  * @retval None
-  */
 void HAL_PPP_MspInit(void)
 {
    
 }
 
-/**
-  * @brief  DeInitializes the PPP MSP.
-  * @retval None
-  */
 void HAL_PPP_MspDeInit(void)
 {
   
 }
-
-/**
-  * @}
-  */
-
-/**
-  * @}
-  */
-
-/**
-  * @}
-  */
-
-
